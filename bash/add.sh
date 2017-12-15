d6a1fd28619866026deb399632055d293f8a7837
Shuya Motouchi <syu.m.5151@gmail.com>
2017-12-15 15:21:07 +0900

    Update add.sh

--- bash/add.sh
+++ bash/add.sh
@@ -1,10 +1,10 @@
 cat > /etc/profile.d/motouchi.sh <<-'__EOS__'
-> #!/bin/bash
-> export HISTSIZE=30000
-> export HISTFILESIZE=30000
-> export HISTIGNORE=rm:shutdown:reboot:halt
-> export HISTCONTROL=ignoreboth
-> export HISTTIMEFORMAT='[%y-%m-%d %H:%M:%S] '
-> # tyo1
-> export PS1='$(if [[ $? == 0 ]]; then echo "\[\e[1;33m\]"; else echo "\[\e[1;31m\]"; fi)$(printf "%*s" $(($(tput cols)-9)) "" | sed "s/ /-/g") \t\n\[\e[1;37m\]\[\e[1;31m\]### HOST by Shuya Motouchi ###\[\e[1;37m\]\n[\[\e[1;35m\]\u@\h\[\e[1;37m\] \w]\$ '
-> __EOS__
+#!/bin/bash
+export HISTSIZE=30000
+export HISTFILESIZE=30000
+export HISTIGNORE=rm:shutdown:reboot:halt
+export HISTCONTROL=ignoreboth
+export HISTTIMEFORMAT='[%y-%m-%d %H:%M:%S] '
+# tyo1
+export PS1='$(if [[ $? == 0 ]]; then echo "\[\e[1;33m\]"; else echo "\[\e[1;31m\]"; fi)$(printf "%*s" $(($(tput cols)-9)) "" | sed "s/ /-/g") \t\n\[\e[1;37m\]\[\e[1;31m\]### HOST by Shuya Motouchi ###\[\e[1;37m\]\n[\[\e[1;35m\]\u@\h\[\e[1;37m\] \w]\$ '
+__EOS__
