c6f8744b11b37cd0f4a81b622e52d9be1775369a
nwiizo <syu.m.5151@gmail.com>
2022-03-28 19:27:10 +0900

    [add] fisher

--- fish/completions/terraform.fish
+++
@@ -1,9 +0,0 @@
-
-function __complete_terraform
-    set -lx COMP_LINE (string join ' ' (commandline -o))
-    test (commandline -ct) = ""
-    and set COMP_LINE "$COMP_LINE "
-    /opt/homebrew/Cellar/tfenv/2.2.2/versions/1.0.0/terraform
-end
-complete -c terraform -a "(__complete_terraform)"
-
