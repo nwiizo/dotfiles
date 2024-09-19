38994e2ffbffba4283e2fb7c4761473865d89858
nwiizo <syu.m.5151@gmail.com>
2024-09-19 12:13:20 +0900

    Add Rust Analyzer to LSP servers list

--- nvchad/lua/configs/lspconfig.lua
+++ nvchad/lua/configs/lspconfig.lua
@@ -1,11 +1,11 @@
--- EXAMPLE 
+-- EXAMPLE
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" ,"golangci_lint_ls", "gopls", "bashls","pylsp","terraform_lsp","terraformls"}
-
+local servers =
+  { "html", "cssls", "golangci_lint_ls", "gopls", "bashls", "pylsp", "terraform_lsp", "terraformls", "rust_analyzer" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -15,11 +15,3 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
-
-
--- typescript
-lspconfig.tsserver.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
